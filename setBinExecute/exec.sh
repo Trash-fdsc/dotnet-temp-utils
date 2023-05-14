fbe06bfc0859600eaba190eee9d742cc9fccd196
fdsc <fdsc@yandex.ru>
2023-05-14 07:13:09 +0300

    automatic commit

--- setBinExecute/exec.sh
+++ setBinExecute/exec.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Применение
+# sudo bash /Arcs/Repos/smalls/dotnet-temp-utils/setBinExecute/exec.sh > /tmp/ex.sh
+# sudo bash /tmp/ex.sh
+
 gold=`pwd`
 # VisualStudioCode:w3geMDnvA18SaWNhn4e5@
 # set +m # disable job control in order to allow lastpipe
@@ -8,10 +13,15 @@ gold=`pwd`
 # dotnet publish --output ./build -c Release --self-contained false --use-current-runtime true /p:PublishSingleFile=true -p:PublishReadyToRun=false
 
 mr=/Arcs/Repos/smalls/dotnet-temp-utils/setBinExecute/build
-whitelist=/Arcs/Repos/smalls/dotnet-temp-utils/setBinExecute/usr.bin.whitelist
 
+# whitelist=/Arcs/Repos/smalls/dotnet-temp-utils/setBinExecute/usr.bin.whitelist
+# $mr/setBinExecute "/usr/bin" "g:noaccess_sbin" "$whitelist"
 
-$mr/setBinExecute "/usr/bin" "g:noaccess_sbin" "$whitelist"
+# whitelist=/Arcs/Repos/smalls/dotnet-temp-utils/setBinExecute/usr.bin.deluge.whitelist
+# $mr/setBinExecute "/usr/bin" "g:noaccess_sbin_deluge" "$whitelist"
+
+whitelist=/Arcs/Repos/smalls/dotnet-temp-utils/setBinExecute/usr.bin.mega.whitelist
+$mr/setBinExecute "/usr/bin" "g:noaccess_sbin_mega" "$whitelist"
 
 
 cd "$gold"
